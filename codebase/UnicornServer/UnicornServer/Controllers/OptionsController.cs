6dc3aa504ea100818f967c92b8d6044c125cc6f8
m.huber <manuel.huber@qaware.de>
2016-06-29 12:26:00 +0200

    Added empty controller

---
+++ codebase/UnicornServer/UnicornServer/Controllers/OptionsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Description;
+using UnicornServer.Models;
+
+namespace UnicornServer.Controllers
+{
+  [RoutePrefix("/options")]
+  public class OptionsController : ApiController
+  {
+
+    [HttpGet]
+    [Route("/unicorns")]
+    [ResponseType(typeof(Option[]))]
+    public IHttpActionResult GetUnicorns ()
+    {
+      return InternalServerError(new Exception("Not yet implemented"));
+    }
+    [HttpGet]
+    [Route("/hats")]
+    [ResponseType(typeof(Option[]))]
+    public IHttpActionResult GetHats()
+    {
+      return InternalServerError(new Exception("Not yet implemented"));
+    }
+    [HttpGet]
+    [Route("/wings")]
+    [ResponseType(typeof(Option[]))]
+    public IHttpActionResult GetWings()
+    {
+      return InternalServerError(new Exception("Not yet implemented"));
+    }
+
+  }
+}
---
+++ codebase/UnicornServer/UnicornServer/Models/Option.cs
@@ -0,0 +1,8 @@
+﻿namespace UnicornServer.Models
+{
+  public class Option
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
