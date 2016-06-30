e4db94c08426cb5f1bd63c412d245f048301657b
m.huber <manuel.huber@qaware.de>
2016-06-30 11:04:50 +0200

    Added unicorns & unicorn controller

--- codebase/UnicornServer/UnicornServer/Controllers/BodiesController.cs
+++ codebase/UnicornServer/UnicornServer/Controllers/BodiesController.cs
@@ -5,6 +5,9 @@ using UnicornServer.Models;
 
 namespace UnicornServer.Controllers
 {
+  /// <summary>
+  /// Rest Endpoint for everything related to unicorn bodies
+  /// </summary>
   [RoutePrefix("v1/bodies")]
   public class BodyController : ApiController
   {
---
+++ codebase/UnicornServer/UnicornServer/Models/Unicorn.cs
@@ -0,0 +1,11 @@
+﻿namespace UnicornServer.Models
+{
+  public class Unicorn
+  {
+    public string Name { get; set; }
+    public int Body { get; set; }
+    public int Hat { get; set; }
+    public int Shoes { get; set; }
+    public int Wings { get; set; }
+  }
+}
