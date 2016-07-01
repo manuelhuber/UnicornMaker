087ab54eb097d1ac20bf701e5b47f150dd25968f
m.huber <manuel.huber@qaware.de>
2016-07-01 10:19:37 +0200

    Added database context

---
+++ codebase/UnicornServer/UnicornServer/Models/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+
+namespace UnicornServer.Models
+{
+  public class DatabaseContext : DbContext
+  {
+    public DatabaseContext() : base("DatabaseContext")
+    {
+    }
+
+    public DbSet<Body> Bodies { get; set; }
+    public DbSet<Hat> Hats { get; set; }
+    public DbSet<Wings> Wings { get; set; }
+    public DbSet<Shoes> Shoes { get; set; }
+  }
+}
--- codebase/UnicornServer/UnicornServer/Models/Option.cs
+++ codebase/UnicornServer/UnicornServer/Models/Option.cs
@@ -1,8 +1,24 @@
 ﻿namespace UnicornServer.Models
 {
-  public class Option
+  public abstract class Option
   {
     public int Id { get; set; }
     public string Name { get; set; }
+  }
+
+  public class Body : Option
+  {
+  }
+
+  public class Hat : Option
+  {
+  }
+
+  public class Wings : Option
+  {
+  }
+
+  public class Shoes : Option
+  {
   }
 }
