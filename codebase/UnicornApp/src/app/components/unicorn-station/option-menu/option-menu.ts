f35f23a36c422804e0dac6356db4012dd39ce320
m.huber <manuel.huber@qaware.de>
2016-07-08 13:56:47 +0200

    Added Option menu entry

--- codebase/UnicornApp/src/app/components/unicorn-station/option-menu/option-menu.ts
+++ codebase/UnicornApp/src/app/components/unicorn-station/option-menu/option-menu.ts
@@ -1,10 +1,13 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
+import {NgFor, NgIf} from '@angular/common'
+import {OptionService} from "../../../services/option-service";
+import {OptionMenuEntry} from "./option-menu-entry/option-menu-entry";
 
 @Component({
   selector: 'option-menu',
   pipes: [],
-  providers: [],
-  directives: [],
+  providers: [OptionService],
+  directives: [OptionMenuEntry, NgFor, NgIf],
   templateUrl: './option-menu.html',
   styleUrls: ['./option-menu.less']
 })
@@ -13,4 +16,28 @@ import {Component, Input} from '@angular/core';
  * Written by Manuel Huber
  */
 export class OptionMenu {
+  bodies : Option[];
+  hats : Option[];
+  wings : Option[];
+  shoes : Option[];
+
+
+  constructor (optionService : OptionService) {
+    optionService.getBodies().subscribe((bodies : Option[]) => {
+      this.bodies = bodies;
+      console.log(this.bodies);
+    });
+    // optionService.getHats().subscribe((hats : Option[]) => {
+    //   this.hats = hats;
+    //   console.log(this.hats);
+    // });
+    // optionService.getWings().subscribe((wings : Option[]) => {
+    //   this.wings = wings;
+    //   console.log(this.wings);
+    // });
+    // optionService.getShoes().subscribe((shoes : Option[]) => {
+    //   this.shoes = shoes;
+    //   console.log(this.shoes);
+    // });
+  }
 }
