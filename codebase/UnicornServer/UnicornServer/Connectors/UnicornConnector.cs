4147ffa1a9eaffa716de1e415e3e1012974e98b9
binarykittenDE <franziska.haaf@gmail.com>
2016-07-11 16:50:52 +0200

    unicorn connector

---
+++ codebase/UnicornServer/UnicornServer/Connectors/UnicornConnector.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using UnicornServer.Models;
+
+namespace UnicornServer.Connectors
+{
+  /// <summary>
+  /// Offers Unicorns specific database access
+  /// Autor: Franziska Haaf
+  /// </summary>
+  public class UnicornConnector
+  {
+    /// <summary>
+    /// The context to access the database
+    /// </summary>
+    public DatabaseContext Context { get; set; }
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    /// <param name="context">To be injected</param>
+    public UnicornConnector(DatabaseContext context)
+    {
+      Context = context;
+    }
+
+    /// <summary>
+    /// Gets all unicorns from the DB
+    /// </summary>
+    /// <returns>A list of Unicorn</returns>
+    public List<Unicorn> GetAllUnicorns()
+    {
+      return Context.Unicorns.ToList();
+    }
+
+    /// <summary>
+    /// Add a unicorn to the DB
+    /// </summary>
+    public void AddUnicorn(Unicorn unicorn)
+    {
+      Context.Unicorns.Add(unicorn);
+    }
+  }
+}
