9259f36f15669eef1dddb62c26a1f41641dd08c9
Manuel Huber <nelo112@web.de>
2016-07-12 17:38:00 +0200

    Added name entry field, some style changes

--- codebase/UnicornApp/src/app/components/unicorn-station/unicorn-station.ts
+++ codebase/UnicornApp/src/app/components/unicorn-station/unicorn-station.ts
@@ -20,6 +20,16 @@ export class UnicornStation {
   @Input()
   previous : Function;
   i : number = 0;
+  private _name : string;
+
+  get name () : string {
+    return this._name;
+  }
+
+  set name (s : string) {
+    this._name = s;
+    this.unicornService.setName(s);
+  }
 
   constructor (private unicornService : UnicornService) {
 
